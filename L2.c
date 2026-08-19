d457ce9307e0588cea133fdf9ef6e3b05ea34be0
shravani-wagh-tech <shravaniw672@gmail.com>
2026-08-19 21:20:59 +0530

    Add files via upload

---
+++ L2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+    int num;
+    int i;
+    printf("enter a number:");
+    scanf("%d",&num);
+    for(i = 1; i <=1; i++)
+    {
+        if(num % 2 == 0)
+        {
+            printf("number is even");
+        }
+        else
+        {
+            printf("number is odd");
+        }
+        }
+    return 0;
+}
