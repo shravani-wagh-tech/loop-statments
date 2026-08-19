19c23b5cfd5a008de5807cf397348af9391439ad
shravani-wagh-tech <shravaniw672@gmail.com>
2026-08-19 21:05:59 +0530

    Add files via upload

---
+++ L1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main()
+{
+    int num;
+    int i;
+    printf("enter a number:");
+    scanf("%d",&num);
+    for(i = 1; i <=1; i++)
+    {
+        if(num>0)
+        {
+            printf("number is positive");
+        }
+        else if(num<0)
+        {
+            printf("number is negative");
+        }
+        else{
+            printf("number is zero");
+        }
+        }
+    return 0;
+}
